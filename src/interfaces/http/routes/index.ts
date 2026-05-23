a5c84821603e513120fedb57d3ffe4d3525ddbfd
Jundi <jundi.nasrullah@mitrakeluarga.com>
2026-05-23 21:59:05 +0700

    chore: remove unused /healthz route

--- src/interfaces/http/routes/index.ts
+++ src/interfaces/http/routes/index.ts
@@ -9,10 +9,6 @@ routers.get('/', (_req: any, res: any) =>
   res.send(`API Service for ${process.env.NODE_ENV}`),
 )
 
-routers.get('/healthz', (_req: any, res: any) =>
-  res.status(200).json({ status: 'ok' }),
-)
-
 routers.use('/v1/ping', ping())
 routers.use('/v1/auth', auth())
 routers.use('/v1/users', user())
